a967a363ce97cfa82fb03506eecc729158afd6f3
edgarserna15 <edgarserna15@hotmail.com>
2015-07-31 07:37:05 -0500

    Testing code

--- PyWebScrapper.py
+++ PyWebScrapper.py
@@ -1,9 +1,22 @@
-
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
 
 class WebScrapper(object):
-    def __init__(self):
 
+    BASE_URL = "http://www.chicagorader.com"
+    
+    def __init__(self):
+        print "hello"
+
+    def get_data_links(self, section_url):
+        html = urlopen(section_url).read()
+        soup = BeautifulSoup(html, "lxml")
+        boccat = soup.find("dl", "boccat")
+        category_links = [BASE_URL + dd.a["href"] for dd in boccat.findAll("dd")]
+        print category_links
+        return category_links
 
 
 if __name__ == "__main__":
-    print "main class"
+    Scrappy = WebScrapper()
+    Data = Scrappy.get_data_links("/chicago/best-of-chicago-2011-food-drink/BestOf?oid=4106228")
