ce1e3f494012ddd54f2afbe171048e2d613cacd3
edgarserna15 <edgarserna15@hotmail.com>
2015-08-19 08:15:02 -0500

    Scrap soccer game info
    Now I'm able to scrap info from the ESPN website. Will add selecting
    date capability and gui interface. Will also add a notification method
    later.

--- PyWebScrapper.py
+++ PyWebScrapper.py
@@ -21,27 +21,63 @@ class WebScrapper(object):
 
     def get_page(self, section_url):
         num = 0
+        count = 1
+        TeamsList = []
+        Goals = []
+        GameNum = []
         League_Score = {}
         WriteToFile = WebScrapper()
         page = urlopen(section_url)
-        soup = BeautifulSoup(page.read())
+        soup = BeautifulSoup(page.read(), 'html.parser')
         league_name = soup.find_all("div", {"class": "score-league"})
-        All_Teams = soup.find_all("div", {"class": "score-group" "score-group first"})
         for league in league_name:
             print(league.h4.get_text())
-            teams = league.find_all("div", {"class": "score-group" "score-group first"})
+            WriteToFile.write_to_file(league.h4.get_text(), "League")
+            GameContent = league.find_all("div", {"class": "score-content"})
+            '''league.find_all("div", {"class": "team-name"})'''
+            for content in GameContent:
+                Teams = content.find_all("div", {"class": "team-name"})
+                Scores = content.find_all("div", {"class": "team-score"})
+                Time = content.find_all("div", {"class": "game-info"})
+                for team in Teams:
+                    team_name = team.span.get_text()
+                    TeamsList.append(team_name.encode('utf8'))
+                for score in Scores:
+                    goals = score.find("span")
+                    string_goals = str(goals)
+                    if count > 2:
+                        pass
+                    else:
+                        amt = string_goals.replace("<span>", "").replace("</span>", "")
+                        Goals.append(amt)
+                    count = count + 1
+                for time in Time:
+                    game_time = time.span.get_text()
+                print "Team 1: " + TeamsList[0] + " Score: " + Goals[0]
+                print "Team 2: " + TeamsList[1] + " Score: " + Goals[1]
+                print "Time  : " + game_time.encode('utf8') + "\n"
+                del TeamsList[:]
+                del Scores[:]
+                count = 1
+                
+                '''WriteToFile.write_to_file(teams.span.get_text(), "Teams")'''
+            
+            '''teams = league.find_all("div", {"class": "score-group" "score-group first"})
             for team in teams.findAll('span'):
-                print team.get_text()
+                print team.get_text()'''
             '''WriteToFile.write_to_file(league.h4.get_text())'''
 
-    def write_to_file(self, data):
+    def write_to_file(self, data, League):
         f = codecs.open('Soccer_Scores.txt', 'a', 'utf-8')
-        f.write(data + "\n")
+        f.write(data)
+        f.close()
+        f = open('Soccer_Scores.txt', 'a')
+        f.write("\n")
         f.close()
 
 if __name__ == "__main__":
     Scrappy = WebScrapper()
-    BASE_URL = "http://www.espnfc.us/scores?date=20150815"
+    BASE_URL = "http://www.espnfc.us/scores?date=20150819"
     Scrappy.get_page(BASE_URL)
     '''url = raw_input("Enter a website to exctract the URL's from: ")
     r = requests.get("http://" + BASE_URL)
