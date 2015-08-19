032000817c4b9a2b15c0d16c25f87fbcb0876b94
edgarserna15 <edgarserna15@hotmail.com>
2015-08-19 14:34:56 -0500

    Removed some lines
    Removed some lines of code that were no longer needed. Now write a file
    as well.

--- PyWebScrapper.py
+++ PyWebScrapper.py
@@ -5,36 +5,25 @@ import codecs
 
 class WebScrapper(object):
 
-    BASE_URL = "http://www.espnfc.us/barclays-premier-league/23/"
-    '''scores?date=20150816'''
+    BASE_URL = "http://www.espnfc.us/scores"
+    '''?date=20150816'''
     
     def __init__(self):
         pass
 
-    def get_data_links(self, section_url):
-        html = urlopen(section_url).read()
-        soup = BeautifulSoup(html, "lxml")
-        boccat = soup.find("dl", "boccat")
-        category_links = [BASE_URL + dd.a["href"] for dd in boccat.findAll("dd")]
-        print category_links
-        return category_links
-
     def get_page(self, section_url):
-        num = 0
         count = 1
         TeamsList = []
         Goals = []
-        GameNum = []
-        League_Score = {}
         WriteToFile = WebScrapper()
+        
         page = urlopen(section_url)
         soup = BeautifulSoup(page.read(), 'html.parser')
         league_name = soup.find_all("div", {"class": "score-league"})
+        
         for league in league_name:
-            print(league.h4.get_text())
-            WriteToFile.write_to_file(league.h4.get_text(), "League")
+            WriteToFile.write_to_file(league.h4.get_text(), "", "", "")
             GameContent = league.find_all("div", {"class": "score-content"})
-            '''league.find_all("div", {"class": "team-name"})'''
             for content in GameContent:
                 Teams = content.find_all("div", {"class": "team-name"})
                 Scores = content.find_all("div", {"class": "team-score"})
@@ -53,38 +42,35 @@ class WebScrapper(object):
                     count = count + 1
                 for time in Time:
                     game_time = time.span.get_text()
-                print "Team 1: " + TeamsList[0] + " Score: " + Goals[0]
-                print "Team 2: " + TeamsList[1] + " Score: " + Goals[1]
-                print "Time  : " + game_time.encode('utf8') + "\n"
+                Team1 =  "Team 1: " + TeamsList[0] + " Score: " + Goals[0]
+                Team2 = "Team 2: " + TeamsList[1] + " Score: " + Goals[1]
+                Time = "Time  : " + game_time.encode('utf8') + "\n"
+                WriteToFile.write_to_file(None, Team1, Team2, Time)
                 del TeamsList[:]
-                del Scores[:]
+                del Goals[:]
                 count = 1
-                
-                '''WriteToFile.write_to_file(teams.span.get_text(), "Teams")'''
-            
-            '''teams = league.find_all("div", {"class": "score-group" "score-group first"})
-            for team in teams.findAll('span'):
-                print team.get_text()'''
-            '''WriteToFile.write_to_file(league.h4.get_text())'''
 
-    def write_to_file(self, data, League):
-        f = codecs.open('Soccer_Scores.txt', 'a', 'utf-8')
-        f.write(data)
-        f.close()
-        f = open('Soccer_Scores.txt', 'a')
-        f.write("\n")
-        f.close()
+    def write_to_file(self, League, Team1, Team2, time):
+        
+        if(League == None):
+            f = open('Soccer_Scores.txt', 'a')
+            f.write(Team1 + "\n" + Team2  + "\n" + time + "\n")
+            f.write("\n")
+            f.close()
+        else:
+            f = codecs.open('Soccer_Scores.txt', 'a', 'utf8')
+            f.write("League: " + League)
+            f.close()
+            f = open('Soccer_Scores.txt', 'a')
+            f.write("\n\n")
+            f.close()
+
 
 if __name__ == "__main__":
     Scrappy = WebScrapper()
-    BASE_URL = "http://www.espnfc.us/scores?date=20150819"
+    BASE_URL = "http://www.espnfc.us/scores?date=20150816"
     Scrappy.get_page(BASE_URL)
-    '''url = raw_input("Enter a website to exctract the URL's from: ")
-    r = requests.get("http://" + BASE_URL)
-    data = r.text
-    soup = BeautifulSoup(data)
-    for link in soup.find_all('a'):
-        print(link.get('href'))'''
+
     
 
 
